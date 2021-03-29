84d7963606ea6fc2a552071b0be5d46a0806e42c
wngq <wqi1203@gmail.com>
2021-03-29 16:01:08 +0800

    2st commit

--- chapter10/10-4.py
+++ chapter10/10-4.py
@@ -1,7 +1,7 @@
 filename = "guest_book.txt"
 prompt = "Please enter you name(enter 'q' to quit!): "
 
-with open(filename, 'a') as file_object:
+with open(filename, 'a') as file_object:  # 写入的时候可以只打开一次文件
     while True:
         name = input(prompt)
         if name != 'q':
