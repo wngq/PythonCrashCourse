bdbbfa8a17d5309ac63b9f70cf894f10a4a67d16
wngq <wqi1203@gmail.com>
2021-03-29 16:09:51 +0800

    3st commit

--- chapter10/10-4.py
+++ chapter10/10-4.py
@@ -1,12 +1,12 @@
 filename = "guest_book.txt"
 prompt = "Please enter you name(enter 'q' to quit!): "
-
+write_avtive = True
 with open(filename, 'a') as file_object:  # 写入的时候可以只打开一次文件
-    while True:
+    while write_avtive:
         name = input(prompt)
         if name != 'q':
             file_object.write(name + '\n')
             print("Hello " + name.title())
         else:
             print("break")
-            break
+            write_avtive = False
