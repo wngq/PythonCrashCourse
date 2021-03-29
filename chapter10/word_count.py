babee1590ad7db1ca021c8a5fd937706e273f3ec
wngq <wqi1203@gmail.com>
2021-03-29 16:58:32 +0800

    1st commit

--- chapter10/word_count.py
+++ chapter10/word_count.py
@@ -1,1 +1,16 @@
-def count_words(filename):    try:        with open(filename) as file_object:            contents = file_object.read()    except FileNotFoundError:        print("Sorry, the file " + filename + " does not exist! ")    else:        words = contents.split()        num_words = len(words)        print(num_words)filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt', 'little_women.txt']for filename in filenames:    count_words(filename)
+def count_words(filename):
+    try:
+        with open(filename) as file_object:
+            contents = file_object.read()
+    except FileNotFoundError:
+        print("Sorry, the file " + filename + " does not exist! ")
+    else:
+        words = contents.split()
+        num_words = len(words)
+        print(num_words)
+
+
+filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt', 'little_women.txt']
+
+for filename in filenames:
+    count_words(filename)
