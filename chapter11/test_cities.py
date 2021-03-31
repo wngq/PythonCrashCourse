ec28b9b71298b4ebf45ebddab12e7bb367feca9f
wngq <wqi1203@gmail.com>
2021-03-31 12:37:16 +0800

    1st commit

--- chapter11/test_cities.py
+++ chapter11/test_cities.py
@@ -13,7 +13,7 @@ class CitiesTestCase(unittest.TestCase):
         self.assertEqual(formatted_ccp, 'Santiago, Chile - population 5000000')
 
 
-# 在Pycharm里面不能用ctrl+shift+r运行测试代码，因为会以unittests模式运行，当前文件名就是不是main了
+# 在Pycharm里面不能用ctrl+shift+r运行测试代码，因为会以unittests模式运行，当前文件名就不是main了
 # ctrl+opt+r，然后再选择要运行的文件
 # print(__name__)
 unittest.main()
