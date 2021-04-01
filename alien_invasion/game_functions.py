5f10a5c2d9351f95df6562c413ec3261dd50d080
wngq <wqi1203@gmail.com>
2021-04-01 13:46:15 +0800

    左右移动飞船

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -3,11 +3,25 @@ import sys
 import pygame
 
 
-def check_events():
+def check_events(ship):
     """监听键盘和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            # 退出游戏
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                # 向右移动飞船
+                ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                # 向左移动飞船
+                ship.moving_left = True
+        elif event.type == pygame.KEYUP:
+            # 按键松开时，停止移动
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 
 def update_screen(ai_settings, screen, ship):
