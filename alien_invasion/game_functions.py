5ab902ebbc3d20251eb6b5a7162ca93c052a288d
wngq <wqi1203@gmail.com>
2021-04-01 23:18:36 +0800

    子弹开火

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -1,15 +1,21 @@
 import sys
 
 import pygame
+from bullet import Bullet
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
         # 向右移动飞船
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         # 向左移动飞船
         ship.moving_left = True
+
+    elif event.key == pygame.K_SPACE:
+        # 创建一颗子弹，并将其加入到编组bullets中
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
@@ -20,23 +26,26 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """监听键盘和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             # 退出游戏
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             # 按键松开时，停止移动
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """更新屏幕上的图像，并切换到新屏幕"""
     # 每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
+    # 在飞船和外星人后面重绘所有子弹
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
 
     # 让最新绘制的屏幕可见
