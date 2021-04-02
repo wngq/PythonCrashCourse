7772723278b2477cf290731e334ecbf6f6ce0948
wngq <wqi1203@gmail.com>
2021-04-02 12:13:39 +0800

    一群外星人

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -17,9 +17,11 @@ def run_game():
     ship = Ship(ai_settings, screen)
     # 创建一个用于存储子弹的编组
     bullets = Group()
+    # 创建一个外星人的编组
+    aliens = Group()
 
-    # 创建一个外星人
-    alien = Alien(ai_settings, screen)
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, aliens)
 
     # 开始游戏的主循环
     while True:
@@ -27,7 +29,7 @@ def run_game():
         ship.update()
         gf.update_bullets(bullets)
 
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
