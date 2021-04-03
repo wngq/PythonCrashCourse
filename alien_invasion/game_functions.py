61f7d50cb89d8b19b0930d51647edb5151a4b7c0
wngq <wqi1203@gmail.com>
2021-04-03 17:32:34 +0800

    13章完结

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -144,19 +144,22 @@ def change_fleet_direction(ai_settings, aliens):
 
 def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     """响应被外星人撞到飞船"""
-    # 将ship_left -= 1
-    stats.ships_left -= 1
+    if stats.ships_left > 0:
+        # 将ship_left -= 1
+        stats.ships_left -= 1
 
-    # 清空外星人列表和子弹列表
-    aliens.empty()
-    bullets.empty()
+        # 清空外星人列表和子弹列表
+        aliens.empty()
+        bullets.empty()
 
-    # 创建一群新的外星人， 并将飞船放到屏幕底端中央
-    create_fleet(ai_settings, screen, ship, aliens)
-    ship.center_ship()
+        # 创建一群新的外星人， 并将飞船放到屏幕底端中央
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
 
-    # 暂停
-    sleep(0.5)
+        # 暂停
+        sleep(0.5)
+    else:
+        stats.game_active = False
 
 
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
@@ -170,3 +173,16 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     if pygame.sprite.spritecollideany(ship, aliens):
         # print("Ship hit!!")
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+
+    # 检测是否有外星人到达屏幕底端
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
+
+
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """检测是否有外星人到达了屏幕底端"""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            # 像飞船被撞到一样处理
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
