dd127ef6dedc964890e782c657afa264b92685b9
wngq <wqi1203@gmail.com>
2021-04-04 11:27:31 +0800

    计分增加plus plus

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -12,7 +12,7 @@ class Settings():
         self.ship_limit = 3
 
         # 子弹设置
-        self.bullet_width = 300
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 9
@@ -22,6 +22,8 @@ class Settings():
 
         # 以什么样的速度加快游戏节奏
         self.speedup_scale = 1.1
+        # 外星人点数的提高速度
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -38,6 +40,10 @@ class Settings():
         self.alien_points = 50
 
     def increase_speed(self):
+        """提高速度设置和外星人点数"""
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        # print(self.alien_points)
