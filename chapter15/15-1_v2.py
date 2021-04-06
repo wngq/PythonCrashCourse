2d1da51545c036cc3491fe13dcffc7142b49fbbd
wngq <wqi1203@gmail.com>
2021-04-06 17:58:22 +0800

    1st commit

---
+++ chapter15/15-1_v2.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+x_values = list(range(1, 5001))
+y_values = [x ** 3 for x in x_values]
+
+plt.scatter(x_values, y_values, edgecolors='none', s=30)
+
+plt.title("Cube Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Cube of Value", fontsize=14)
+plt.tick_params(axis='both', which='major', labelsize=14)
+
+plt.show()
