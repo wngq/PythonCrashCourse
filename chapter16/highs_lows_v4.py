2041bb27f1b408925d373632e6baa3101b8c85db
wngq <wqi1203@gmail.com>
2021-04-09 14:20:03 +0800

    1st commit

--- chapter16/highs_lows_v4.py
+++ chapter16/highs_lows_v4.py
@@ -11,21 +11,24 @@ with open(filename) as f:
     # print(header_row)
     # for index, column_header in enumerate(header_row):
     #     print(index, column_header)
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], '%Y-%m-%d')
         # print(current_date)
         dates.append(current_date)
         highs.append(int(row[1]))
+        lows.append(int(row[3]))
     # print(dates)
     # print(highs)
+    # print(lows)
 
 # 根据数据绘制图形
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, c='red')
+plt.plot(dates, lows, c='blue')
 
 # 设置图形格式
-plt.title("Daily high temperature - 2014", fontsize=24)
+plt.title("Daily high and low temperature - 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()  # 绘制斜的日期标签
 plt.ylabel('Temperature(F)', fontsize=16)
