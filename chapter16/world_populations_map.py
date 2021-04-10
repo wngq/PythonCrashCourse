63f6239824d0aac7fd6d6d220b706fe22090b6ce
wngq <wqi1203@gmail.com>
2021-04-10 14:09:01 +0800

    1st commit

--- chapter16/world_populations_map.py
+++ chapter16/world_populations_map.py
@@ -1,6 +1,7 @@
 import json
 from country_codes import get_country_code
 import pygal_maps_world.maps
+from pygal.style import RotateStyle, LightColorizedStyle
 
 filename = 'population_data.json'
 
@@ -16,8 +17,24 @@ for pop_dict in pop_data:
         if code:
             cc_population[code] = population
 
-wm = pygal_maps_world.maps.World()
+# 根据人口数量将所有国家分组
+cc_pop_1, cc_pop_2, cc_pop_3 = {}, {}, {}
+for cc, pop in cc_population.items():
+    if pop < 10000000:
+        cc_pop_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pop_2[cc] = pop
+    else:
+        cc_pop_3[cc] = pop
+
+# 看看每组分别包括多少个国家
+print(len(cc_pop_1), len(cc_pop_2), len(cc_pop_3))
+
+wm_style = RotateStyle('#336699', base_style=LightColorizedStyle)
+wm = pygal_maps_world.maps.World(style=wm_style)
 wm.title = 'Populations of World in 2010, by Country'
-wm.add('World 2010', cc_population)
+wm.add('0-10m', cc_pop_1)
+wm.add('10m-1bn', cc_pop_2)
+wm.add('>1bn', cc_pop_3)
 
 wm.render_to_file('world_populations.svg')
